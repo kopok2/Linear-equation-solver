c3b277102528ad8b9bcc3c3254f381465e41d856
Karol Oleszek <oleszek.karol@gmail.com>
2019-03-16 12:09:34 +0100

    Fixed docstrings

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+"""System of linear equation solver application module.
+
+Features:
+    - parsing and solving user-entered systems of linear equations
+    - generating random systems of linear equations
+    - saving and loading equations from JSON files
+    - visualizing equation variables
+
+Prequisities:
+    - tkinter
+    - json
+    - matplotlib
+    - numpy
+"""
+
 import tkinter
 import matplotlib.pyplot as plt
 from tkinter import messagebox, filedialog
@@ -5,45 +20,60 @@ from solver import Equation, floatize_fractions2d
 
 eq = Equation()
 
+
 def about():
+    """Show information about application."""
     messagebox.showinfo("About", "System of linear equations solver\nVersion 1.0\n\nCreated by Karol Oleszek and Krzysztof Olipra")
 
+
 def solve_entered():
+    """Parse and solve user-entered equations."""
     try:
         eq.load_from_string(MATRIX_DISPLAY.get("1.0", tkinter.END))
         result = str(eq)
-        MATRIX_DISPLAY.delete("1.0",tkinter.END)
+        MATRIX_DISPLAY.delete("1.0", tkinter.END)
         MATRIX_DISPLAY.insert("1.0", result)
     except:
         messagebox.showinfo("Parsing error", "Wrong equation format. Please provide equation in correct format:\n1x1 +2x2 +3x3 = 4\n1x1 +2x2 +3x3 = 4\n1x1 +2x2 +3x3 = 4")    
 
+
 def save_to_file():
+    """Save current equations to file."""
     path = filedialog.asksaveasfilename(defaultextension=".json")
     eq.save_to_file(str(path))
 
+
 def load_from_file():
+    """Load equations from file."""
     path = filedialog.askopenfilename()
     eq.load_from_file(str(path))
     result = str(eq)
     MATRIX_DISPLAY.delete("1.0",tkinter.END)
     MATRIX_DISPLAY.insert("1.0", result)
 
+
 def randomize():
+    """Generate random equations."""
     eq.generate_random()
     result = str(eq)
     MATRIX_DISPLAY.delete("1.0",tkinter.END)
     MATRIX_DISPLAY.insert("1.0", result)
 
+
 def visualize():
+    """Visualize variable attributes."""
     plt.pcolor(floatize_fractions2d(eq.variable_matrix))
     plt.show()
 
+
+# Main form initialization
 MAIN_FORM = tkinter.Tk()
 MAIN_FORM.state('zoomed')
 MAIN_FORM.title('System of linear equations solver')
 MATRIX_DISPLAY = tkinter.Text(MAIN_FORM)
 MATRIX_DISPLAY.pack(expand=True, fill='both')
 
+# Menu initialization
 MENUBAR = tkinter.Menu(MAIN_FORM)
 MATRIX_MENU = tkinter.Menu(MENUBAR, tearoff = 0)
 MATRIX_MENU.add_command(label="Solve entered equation", command = solve_entered)
@@ -57,4 +87,5 @@ MENUBAR.add_cascade(label = "Equation", menu = MATRIX_MENU)
 MENUBAR.add_command(label = "About", command = about)
 MAIN_FORM.config(menu = MENUBAR)
 
-MAIN_FORM.mainloop()                
+# Run an application
+MAIN_FORM.mainloop()
--- solver.py
+++ solver.py
@@ -7,22 +7,66 @@ from fractions import Fraction
 from lu_decomposer import lu_decomposition
 
 def floatize_fractions1d(vector):
+    """Convert vector to float vector.
+
+    Args:
+        vector: list of numbers.
+
+    Returns:
+        list of floats.
+    """
     return [float(x) for x in vector]
 
 
 def floatize_fractions2d(matrix):
+    """Convert matrix to float matrix.
+
+    Args:
+        matrix: matrix of numbers.
+
+    Returns:
+        matrix of floats.
+    """
     return [[float(x) for x in row]for row in matrix]
 
     
 def defloatize_fractions1d(vector):
+    """Convert vector to fraction vector.
+
+    Args:
+        vector: list of numbers.
+
+    Returns:
+        list of fraction.
+    """
     return [Fraction(x) for x in vector]
 
 
 def defloatize_fractions2d(matrix):
+    """Convert matrix to fraction matrix.
+
+    Args:
+        matrix: matrix of numbers.
+
+    Returns:
+        matrix of fractions.
+    """
     return [[Fraction(x) for x in row]for row in matrix]
 
 
 def forward_substitution(variable_matrix, result_vector):
+    """Perform a forward_substitution.
+        Solves vectorized equation:
+        Ax =Y
+        and gives x values.
+
+    Args:
+        variable_matrix: lower triangular matrix.
+        result_vector: list of numbers.
+
+    Returns:
+        equation result.
+    """
     result = []
     for x in range(result_vector.__len__()):
         sum = 0
@@ -33,6 +77,18 @@ def forward_substitution(variable_matrix, result_vector):
 
 
 def backward_substitution(variable_matrix, result_vector):
+    """Perform a forward_substitution.
+        Solves vectorized equation:
+        Ax =Y
+        and gives x values.
+
+    Args:
+        variable_matrix: upper triangular matrix.
+        result_vector: list of numbers.
+
+    Returns:
+        equation result.
+    """
     new_matrix = []
     new_results = []
     for row_id in range(variable_matrix.__len__()):
@@ -43,10 +99,22 @@ def backward_substitution(variable_matrix, result_vector):
 
 class Equation:
     def __init__(self, variable_matrix=[[1]], result_vector=[[1]]):
+        """Create an equation.
+            Ax = Y
+
+        Args:
+            variable_matrix: lower triangular matrix.
+            result_vector: list of numbers.
+        """
         self.variable_matrix = variable_matrix
         self.result_vector = result_vector
 
     def __str__(self):
+        """Show equation in displayable form.
+        
+        Returns:
+            equation string represantion (str).
+        """
         results = self.solve()
         to_print = 'System of linear equations:\n\n'
         for x in range(self.variable_matrix.__len__()):
@@ -63,6 +131,11 @@ class Equation:
         return to_print
 
     def load_from_string(self, source):
+        """Parse string to equation.
+
+        Args:
+            source: equation in string form (str).
+        """
         lines = source.split("\n")
         lines = list(filter(None, lines))
         size = lines.__len__()
@@ -85,6 +158,11 @@ class Equation:
         self.result_vector = result_vector
             
     def save_to_file(self, path):
+        """Save equations to file.
+
+        Args:
+            path: path to file where it will be saved.
+        """
         out_dict = {'Matrix': floatize_fractions2d(self.variable_matrix), 
                     'Result': floatize_fractions1d(self.result_vector),
                     'XVector': floatize_fractions1d(self.solve())}
@@ -92,16 +170,33 @@ class Equation:
             json.dump(out_dict, outfile, indent=2)
 
     def load_from_file(self, path):
+        """Load equations from file.
+
+        Args:
+            path: path to file where it is saved.
+        """
         with open(path, "r") as json_file:
             data = json.load(json_file)
             self.variable_matrix = defloatize_fractions2d(data['Matrix'])
             self.result_vector = defloatize_fractions1d(data['Result'])
 
     def generate_random(self, size=8, lowerlimit=-50 ,upperlimit=50):
+        """Generate random equations.
+
+        Args:
+            size: number of equations to generate (int).
+            lowerlimit: lowest possible attribute in equations (number).
+            upperlimit: highest possible attribute in equations (number).
+        """
         self.variable_matrix = [[random.randint(lowerlimit,upperlimit) for x in range(size)] for y in range(size)]
         self.result_vector = [random.randint(lowerlimit,upperlimit) for x in range(size)]
 
     def solve(self):
+        """Solve a system of equations.
+
+        Returns:
+            vector of variable values (list of numbers).
+        """
         factorized = lu_decomposition(self.variable_matrix)
         z = forward_substitution(factorized[0], self.result_vector)
         return backward_substitution(factorized[1], z)
